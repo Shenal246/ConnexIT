dd4ca6f2c9bc8872f6e366c26e5adccd4abb776c
Shenal Perera <pererashenal369@gmail.com>
2024-06-28 10:30:45 +0530

    Updated Thaisolutions

--- src/Thailand/Components/Solutions/Solutions.js
+++ src/Thailand/Components/Solutions/Solutions.js
@@ -16,7 +16,7 @@ import mrcst from '../../../images/pillers/Microsoft.jpg';
 
 function Solutions() {
   const { t } = useTranslation();
-  const { maintopic, periTopic, cyberTopic, authTopic, secTopic, endTopic, netTopic, dataTopic, serverTopic, Microsoft } = t('solutionssec', { returnObjects: true });
+  const { Thaimaintopic, ThaiperiTopic, ThaicyberTopic, ThaiauthTopic, ThaisecTopic, ThaiendTopic, ThainetTopic, ThaidataTopic, ThaiserverTopic, ThaiMicrosoft } = t('Thaisolutionssec', { returnObjects: true });
 
 
   return (
@@ -28,7 +28,7 @@ function Solutions() {
           <div className='row'>
             <div className="row text">
               <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
-              <div className="col-4" data-aos="fade-up" data-aos-delay="100"><p id='topic'>{maintopic}</p></div>
+              <div className="col-4" data-aos="fade-up" data-aos-delay="100"><p id='topic'>{Thaimaintopic}</p></div>
               <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
             </div>
           </div>
@@ -38,7 +38,7 @@ function Solutions() {
                 <div className="card crd">
                   <img src={Perimie} className="card-img-top" alt="Perimeter and Internal Security" />
                   <div className="card-body cardSize">
-                    <p className="card-text txts">{periTopic}</p>
+                    <p className="card-text txts">{ThaiperiTopic}</p>
                   </div>
                 </div>
               </NavLink>
@@ -48,7 +48,7 @@ function Solutions() {
                 <div className="card crd">
                   <img src={cyber} className="card-img-top" alt="Cyber Security Governance & Compliance" />
                   <div className="card-body cardSize">
-                    <p className="card-text txts">{cyberTopic}</p>
+                    <p className="card-text txts">{ThaicyberTopic}</p>
                   </div>
                 </div>
               </NavLink>
@@ -58,7 +58,7 @@ function Solutions() {
                 <div className="card crd">
                   <img src={Authitianit} className="card-img-top" alt="Authentication & Identity Management" />
                   <div className="card-body cardSize">
-                    <p className="card-text txts">{authTopic}</p>
+                    <p className="card-text txts">{ThaiauthTopic}</p>
                   </div>
                 </div>
               </NavLink>
@@ -71,7 +71,7 @@ function Solutions() {
                 <div className="card crd">
                   <img src={Security} className="card-img-top" alt="Security Management" />
                   <div className="card-body cardSize1">
-                    <p className="card-text txts">{secTopic}</p>
+                    <p className="card-text txts">{ThaisecTopic}</p>
                   </div>
                 </div>
               </NavLink>
@@ -81,7 +81,7 @@ function Solutions() {
                 <div className="card crd">
                   <img src={datapoint} className="card-img-top" alt="Endpoint Security" />
                   <div className="card-body cardSize1">
-                    <p className="card-text txts">{endTopic}</p>
+                    <p className="card-text txts">{ThaiendTopic}</p>
                   </div>
                 </div>
               </NavLink>
@@ -91,7 +91,7 @@ function Solutions() {
                 <div className="card crd">
                   <img src={networking} className="card-img-top" alt="Networking" />
                   <div className="card-body cardSize1">
-                    <p className="card-text txts">{netTopic}</p>
+                    <p className="card-text txts">{ThainetTopic}</p>
                   </div>
                 </div>
               </NavLink>
@@ -103,7 +103,7 @@ function Solutions() {
               <div className="card crd">
                 <img src={endpoint} className="card-img-top endpoint" alt="Data Center Infrastructure and Infrastructure Monitoring" />
                 <div className="card-body cardSize1">
-                  <p className="card-text txts">{dataTopic}</p>
+                  <p className="card-text txts">{ThaidataTopic}</p>
                 </div>
               </div>
             </NavLink>
@@ -113,7 +113,7 @@ function Solutions() {
                 <div className="card crd ">
                   <img src={servers} className="card-img-top solImageHight" alt="Server Storage & Backup Solutions" />
                   <div className="card-body cardSize1">
-                    <p className="card-text txts">{serverTopic}</p>
+                    <p className="card-text txts">{ThaiserverTopic}</p>
                   </div>
                 </div>
               </NavLink>
@@ -123,7 +123,7 @@ function Solutions() {
                 <div className="card crd ">
                   <img src={mrcst} className="card-img-top solImageHight" alt="Microsoft" />
                   <div className="card-body cardSize1">
-                    <p className="card-text txts">{Microsoft}</p>
+                    <p className="card-text txts">{ThaiMicrosoft}</p>
                   </div>
                 </div>
               </NavLink>

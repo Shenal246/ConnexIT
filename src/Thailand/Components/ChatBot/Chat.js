6e4d16ada123a067ac6d37039199478e803fe597
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-08-20 09:16:30 +0530

    commmit

--- src/Thailand/Components/ChatBot/Chat.js
+++ src/Thailand/Components/ChatBot/Chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './Chat.css';
 import ChatBot from 'react-simple-chatbot';
-import Botpic from '../../../images/chatprof.png';
+import Botpic1 from '../../../images/Chat/chat1.jpg';
 import axios from "axios";
 import { useTranslation } from 'react-i18next';
 import connections from '../../../config';
@@ -53,7 +53,7 @@ function Chat() {
   }, []);
 
   const images = [
-    Botpic, Botpic
+    Botpic1, Botpic1
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -113,7 +113,7 @@ function Chat() {
         floating={true}
         handleEnd={handleEnd}
         className="custom-chatbot"
-        botAvatar={Botpic}
+        botAvatar={Botpic1}
         headerTitle="Kiara"
       />
       <div className="online-dot"></div>

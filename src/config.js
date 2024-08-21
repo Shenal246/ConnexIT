82164776076866c8d99aa9c78c1a34b8a8dacf48
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-08-21 12:01:18 +0530

    Commit
    Contact us srilanka site API

--- src/config.js
+++ src/config.js
@@ -1,18 +1,30 @@
 const config = {
-    serverLink: 'https://api.connexit.biz/search',
-    serverLinkInsert: 'https://api.connexit.biz/insert',
-    slupcoming: 'https://api.connexit.biz/slupev',
-    camupcoming: 'https://api.connexit.biz/camupev',
-    thaiupcoming: 'https://api.connexit.biz/taiupev',
-    slupreg: 'https://api.connexit.biz/upcomreg',
-    slchatinsert: 'https://api.connexit.biz/newchat',
-    slseatUpdate: 'https://api.connexit.biz/seatupdate',
-    slupcomingseat: 'https://api.connexit.biz/slupevst',
-    slvendorfetch: 'https://api.connexit.biz/logoforcnt1',
-    camBlog: 'https://api.connexit.biz/camblog',
-    auupcoming: '',
-    muupcoming: '',
-    bruupcoming: ''
+    // serverLink: 'https://api.connexit.biz/search',
+    // serverLinkInsert: 'https://api.connexit.biz/insert',
+    // slupcoming: 'https://api.connexit.biz/slupev',
+    // camupcoming: 'https://api.connexit.biz/camupev',
+    // thaiupcoming: 'https://api.connexit.biz/taiupev',
+    // slupreg: 'https://api.connexit.biz/upcomreg',
+    // slchatinsert: 'https://api.connexit.biz/newchat',
+    // slseatUpdate: 'https://api.connexit.biz/seatupdate',
+    // slupcomingseat: 'https://api.connexit.biz/slupevst',
+    // slvendorfetch: 'https://api.connexit.biz/logoforcnt1',
+    // camBlog: 'https://api.connexit.biz/camblog',
+    // auupcoming: '',
+    // muupcoming: '',
+    // bruupcoming: '',
+
+
+
+
+    newcontactus: 'http://192.168.13.163:5000/NewContactUs',
+
+
+
+
+
+
 };
 
 export default config;
+ 

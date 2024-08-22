8b39e5eb1adf42b81bbd2a2b3c3db577e80ce6d6
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-08-22 11:38:50 +0530

    Update Latest.js

--- src/Mauritius/Components/Home/LatestEvents/Latest.js
+++ src/Mauritius/Components/Home/LatestEvents/Latest.js
@@ -134,7 +134,7 @@ const Latest = () => {
                     {/* See more button */}
                     <div className="row" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="50">
                         <div className="navigation">
-                            <NavLink to="/SL/Events&News"><Button variant="" className='iconsss' style={{ backgroundColor: '#01066B' }}>
+                            <NavLink to="/MU/Events&News"><Button variant="" className='iconsss' style={{ backgroundColor: '#01066B' }}>
                                 <BsArrowRight size={30} />
                             </Button></NavLink>
                         </div>

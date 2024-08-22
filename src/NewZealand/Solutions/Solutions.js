b956184abdf8b09f6fda1a647c636e0dd0e207db
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-08-22 17:17:08 +0530

    NZ Site

---
+++ src/NewZealand/Solutions/Solutions.js
@@ -0,0 +1,124 @@
+import './Solutions.css';
+import Authitianit from '../../images/pillers/Authitianit.jpg';
+import cyber from '../../images/pillers/cyber.jpg';
+import endpoint from '../../images/pillers/endpoint.jpg';
+import datapoint from '../../images/pillers/Datapoint.jpg';
+import Security from '../../images/pillers/Security.jpg';
+import networking from '../../images/pillers/networking.jpg';
+import Perimie from '../../images/pillers/Perimie.jpg';
+import servers from '../../images/pillers/severBackup.jpg';
+import { NavLink } from "react-router-dom";
+import NZNavbar from "../Navbar/Navbar";
+import NZChat from "../ChatBot/Chat";
+import React from "react";
+import NZFooter from "../Footer/Footer";
+
+function Solutions() {
+  return (
+    <>
+      <NZNavbar />
+      <NZChat />
+      <section id='hero' className='hero1 heroSolution'>
+        <div className="container text-center">
+          <div className='row'>
+            <div className="row text">
+              <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
+              <div className="col-4" data-aos="fade-up" data-aos-delay="100"><p id='topic'>Solutions</p></div>
+              <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
+            </div>
+          </div>
+
+          <div className="row row-cols-1 row-cols-lg-4 g-2 g-lg-3 rowmargin1234">
+            <div className="col">
+              <NavLink to="/NZ/Solutions/Perimia" activeClassName='active-link'>
+                <div className="card crd">
+                  <img src={Perimie} className="card-img-top" alt="Perimeter and Internal Security" />
+                  <div className="card-body cardSize">
+                    <p className="card-text txts">Perimeter and <br /> Internal Security</p>
+                  </div>
+                </div>
+              </NavLink>
+            </div>
+            <div className="col">
+              <NavLink to="/NZ/Solutions/CyberSecurity" activeClassName='active-link'>
+                <div className="card crd">
+                  <img src={cyber} className="card-img-top" alt="Cyber Security Governance & Compliance" />
+                  <div className="card-body cardSize">
+                    <p className="card-text txts">Cyber Security Governance <br />  & Compliance</p>
+                  </div>
+                </div>
+              </NavLink>
+            </div>
+            <div className="col">
+              <NavLink to="/NZ/Solutions/Authentication" activeClassName='active-link'>
+                <div className="card crd">
+                  <img src={Authitianit} className="card-img-top" alt="Authentication & Identity Management" />
+                  <div className="card-body cardSize">
+                    <p className="card-text txts">Authentication & Identity <br /> Management</p>
+                  </div>
+                </div>
+              </NavLink>
+            </div>
+            <div className="col">
+              <NavLink to="/NZ/Solutions/SecurityManagement" activeClassName='active-link'>
+                <div className="card crd">
+                  <img src={Security} className="card-img-top" alt="Security Management" />
+                  <div className="card-body cardSize1">
+                    <p className="card-text txts">Security Management</p>
+                  </div>
+                </div>
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="row row-cols-1 row-cols-lg-4 g-2 g-lg-3 rowmargin1234">
+            <div className="col">
+              <NavLink to="/NZ/Solutions/EndpointSecurity" activeClassName='active-link'>
+                <div className="card crd">
+                  <img src={datapoint} className="card-img-top" alt="Endpoint Security" />
+                  <div className="card-body cardSize1">
+                    <p className="card-text txts">Endpoint Security</p>
+                  </div>
+                </div>
+              </NavLink>
+            </div>
+            <div className="col">
+              <NavLink to="/NZ/Solutions/Networking" activeClassName='active-link'>
+                <div className="card crd">
+                  <img src={networking} className="card-img-top" alt="Networking" />
+                  <div className="card-body cardSize1">
+                    <p className="card-text txts">Networking</p>
+                  </div>
+                </div>
+              </NavLink>
+            </div>
+            <div className="col">
+              <NavLink to="/NZ/Solutions/Infrastructure" activeClassName='active-link'>
+                <div className="card crd">
+                  <img src={endpoint} className="card-img-top endpoint111" alt="Data Center Infrastructure and Infrastructure Monitoring" />
+                  <div className="card-body cardSize1">
+                    <p className="card-text txts endpt">Data Center Infrastructure and Monitoring</p>
+                  </div>
+                </div>
+              </NavLink>
+            </div>
+            <div className="col">
+              <NavLink to="/NZ/Solutions/Server" activeClassName='active-link'>
+                <div className="card crd">
+                  <img src={servers} className="card-img-top endpoint111" alt="Server Storage & Backup Solutions" />
+                  <div className="card-body cardSize1">
+                    <p className="card-text txts">Server Storage & Backup Solutions</p>
+                  </div>
+                </div>
+              </NavLink>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <NZFooter />
+    </>
+  );
+}
+
+export default Solutions;
---
+++ src/NewZealand/Solutions/solutionVendors/Perimia/Perimia.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './Perimia.css';
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { Row } from 'react-bootstrap';
+import connections from '../../../../config';
+import { useNavigate } from 'react-router-dom';
+import NZNavbar from "../../../Navbar/Navbar";
+import NZChat from "../../../ChatBot/Chat";
+import NZFooter from "../../../Footer/Footer";
+
+function Perimia() {
+    const [vendors, setVendors] = useState([]);
+    const [show, setShow] = useState(false);
+    const [currentVendor, setCurrentVendor] = useState(null);
+    const navigate = useNavigate();
+    const serverlink = connections.pillor1;
+
+    useEffect(() => {
+        axios.get(serverlink, {headers:{cnt:7}}).then((response) => {
+            setVendors(response.data);
+            console.log(vendors);
+        }).catch((err) => {
+            console.log(err);
+        });
+
+        console.log(vendors);
+
+    }, []);
+
+    const handleCardClick = (vend) => {
+        navigate(`/NZ/Solutions/Perimia/${vend.name}`, { state: { vend } });
+    };
+
+    return (
+        <>
+            <NZNavbar />
+            <NZChat />
+            <section className='heroSolution'>
+                <div className="container">
+                    <div className='row '>
+                        <div className="row text headingrow">
+                            <div className="col-4" data-aos="fade-up" data-aos-delay="100">
+                                <hr/>
+                            </div>
+                            <div className="col-4" data-aos="fade-up" data-aos-delay="100"><p id='topic'>Perimeter And
+                                Internal Security</p></div>
+                            <div className="col-4" data-aos="fade-up" data-aos-delay="100">
+                                <hr/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 rowmargin">
+
+                        {vendors && vendors.map((vend, index) => (
+                            <div className="col" key={index}>
+
+                                <div className="card h-100" onClick={() => {
+                                    handleCardClick(vend);
+                                }}>
+                                    {vend.image_data ? (
+                                        <>
+                                            <img
+                                                src={`data:image/jpeg;base64,${vend.image_data}`}
+                                                alt={vend.title}
+                                                className="card-img-top"
+
+                                            />
+                                        </>
+                                    ) : (
+                                        <p>No Image Available</p>
+                                    )}
+                                    <div className="card-body">
+                                        <h5 className="card-title vendorTitel">{vend.name}</h5>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        ))}
+                    </div>
+
+
+                    {/* <SingleVendor vendorName={currentVendor && currentVendor.name} imageSrc={currentVendor && currentVendor.image_data} description={currentVendor && currentVendor.des} websiteLink={currentVendor && currentVendor.wlink}/> */}
+
+                </div>
+            </section>
+            <NZFooter />
+        </>
+    );
+}
+
+export default Perimia;
